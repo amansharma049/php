a2bb04b1b4a5d73a37bac0317c8a138d004c514c
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2022-10-10 09:58:26 +0530

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -1,5 +1,2 @@
-
-
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is : <?php echo $_GET["email"]; ?>
-
+<h1>Welcome <?php echo $_POST["name"]; ?></h1><br>
+<h1>Your email address is: </h1> <?php echo $_POST["email"]; ?>
